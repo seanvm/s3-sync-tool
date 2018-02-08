8ef19db95e6bc9e50aeae4074b76537f0aa6bf88
Seanvm <Sean@vanmulligen.ca>
2018-02-07 21:50:23 -0800

    Setup bucket selector dropdown

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import BucketSelector from './components/BucketSelector';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedBucket: '',
+      currentDirectory: ''
+    };
+  }
+  
+  handleBucket = (bucket) => {
+    this.setState({selectedBucket: bucket})
+    console.log(this.state)
+  }
+  
   render() {
+    console.log(__dirname);
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BucketSelector onSelectBucket={this.handleBucket} />
+         
+        <div>
+          {this.state.selectedBucket}  
+        </div>
       </div>
     );
   }
---
+++ src/components/BucketSelector.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+// import { PropagateLoader } from 'react-spinners';
+
+const execAsync = window.require('async-child-process').execAsync;
+
+class BucketSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buckets: [],
+      loading: true
+    };
+  }
+  
+  componentDidMount() {
+    this.getBuckets();
+  }
+  
+  handleBucketChange = (event) => {
+    console.log(event.target.value)
+    this.props.onSelectBucket(event.target.value);
+  } 
+  
+  getBuckets() {
+    return execAsync("aws s3 ls").then(results => {
+      return this.parseBuckets(results.stdout);
+    });
+  }
+  
+  parseBuckets(bucketString) {
+    console.log(bucketString)
+    var stringArray = bucketString.split(/(\s+)/);
+    var buckets = stringArray.filter(function(element){
+      element = element.replace(":", "");
+      return !(Date.parse(element) || (element.trim().length == 0) || (element == "\n") || (parseInt(element)));
+    });
+    var loading = false;
+    this.setState({ buckets, loading });
+  }
+  
+  render() {
+    var bucketOptions = this.state.buckets.map(function(item) {
+      return (
+        <option key={item} value={item}>{item}</option>
+      );
+    });
+    
+    return (
+      <div className="container-fluid">
+        <FormGroup>
+          <Label for="exampleSelect">Select A Bucket</Label>
+          <Input type="select" name="select" id="exampleSelect" onChange={this.handleBucketChange}>
+            <option value="" disabled selected>{this.state.loading ? 'Loading' : 'Select an S3 bucket'}</option>
+            {bucketOptions}
+          </Input>
+        </FormGroup>
+      </div>
+    );
+  }
+}
+
+export default BucketSelector;
