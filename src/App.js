5471fd3998248cb4f5c1f5790c3e42592a876d75
Seanvm <Sean@vanmulligen.ca>
2019-03-22 00:06:50 -0700

    Add sidebar to main layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BucketSelector from './components/BucketSelector';
 import BucketSync from './components/BucketSync';
 import Notifications from './components/Notifications';
+import Sidebar from './components/Sidebar';
 import { Button } from 'reactstrap';
 import './App.css';
 import BucketStats from './components/BucketStats';
@@ -39,23 +40,31 @@ class App extends Component {
   
   render() { 
     return (
-      <div className="App">
-        <Notifications alertMessage={this.state.alertMessage} />
-        <BucketSelector onSelectBucket={this.handleBucket} />
-        <div className="container-fluid">
-          <div className="row bottom-buffer">
-            <div className="col-md-4 text-left">
-              <Button onClick={() => this.selectDirectory()}>Choose Download Directory</Button>{' '}
-            </div>
-            
-            <div className="col-md-4 text-left">
-              {this.state.downloadDirectory.length ? `Current Directory: ${this.state.downloadDirectory}` : 'No Directory Selected'}
-            </div>
-            <BucketStats selectedBucket={this.state.selectedBucket} />
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          <div className="sidebar col-2 col-lg-1 col-xs-6 px-1 position-fixed h-100">
+            <Sidebar />
           </div>
-          
-          {/* Download Button + Console output */}
-          <BucketSync selectedBucket={this.state.selectedBucket} downloadDirectory={this.state.downloadDirectory} handleAlertMessage={this.handleAlertMessage} />
+
+          <div className="main col offset-2 offset-lg-1 offset-xs-6 h-100">
+            <Notifications alertMessage={this.state.alertMessage} />
+            <BucketSelector onSelectBucket={this.handleBucket} />
+            <div className="container-fluid">
+              <div className="row bottom-buffer">
+                <div className="col-md-4 text-left">
+                  <Button onClick={() => this.selectDirectory()}>Choose Download Directory</Button>{' '}
+                </div>
+                
+                <div className="col-md-4 text-left">
+                  {this.state.downloadDirectory.length ? `Current Directory: ${this.state.downloadDirectory}` : 'No Directory Selected'}
+                </div>
+              </div>
+              <BucketStats selectedBucket={this.state.selectedBucket} />
+              
+              <BucketSync selectedBucket={this.state.selectedBucket} downloadDirectory={this.state.downloadDirectory} handleAlertMessage={this.handleAlertMessage} />
+            
+            </div>
+          </div>
         </div>
       </div>
     );
